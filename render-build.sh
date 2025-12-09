250028081fb7f5360301fb498f00edaf9034d006
corpatlasone-creator <corp.atlasone@gmail.com>
2025-12-09 12:38:20 -0300

    render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -2,11 +2,11 @@
 # O comando abaixo faz o processo parar se der qualquer erro
 set -o errexit
 
-# 1. Atualiza o pip e instala as bibliotecas
+# 1. Atualiza o pip e instala as dependências
 pip install --upgrade pip
 pip install -r requirements.txt
 
-# 2. Instala o navegador Chromium E o componente Headless Shell (IMPORTANTE)
+# 2. Instala o navegador e a PEÇA QUE ESTAVA FALTANDO
 playwright install chromium
 playwright install chromium-headless-shell
 playwright install-deps
