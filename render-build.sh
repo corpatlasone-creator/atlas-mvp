89f1b15ebcdbd5c18f9868728d02ff1ac8c34934
corpatlasone-creator <corp.atlasone@gmail.com>
2025-12-09 11:25:34 -0300

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -2,9 +2,10 @@
 # O comando abaixo faz o processo parar se der qualquer erro
 set -o errexit
 
-# 1. Instala as dependências do Python (Streamlit, Pandas, etc)
+# 1. Atualiza o pip e instala as dependências (Streamlit, Pandas, etc)
+pip install --upgrade pip
 pip install -r requirements.txt
 
 # 2. Instala APENAS o navegador Chromium do Playwright
-# (Isso evita o erro de senha "su" e garante que o arquivo exista)
+# (Isso garante que o arquivo executável exista)
 playwright install chromium
